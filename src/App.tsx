4f84d72587a5e72d8af8ba3e157a7e7acab00afc
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-06-01 19:21:52 +0200

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>All about Images</h1>
       <div className="card">
         <button onClick={() => upload(images)}>Upload</button>
       </div>
