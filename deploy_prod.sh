e05258d86f020ba4d83c41e5fff519a298225fd5
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-06-08 00:19:07 +0200

    update action

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -21,6 +21,8 @@ AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":true,\
 \"profileName\":\"default\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
 \"region\":\"eu-west-1\"\
 }"
 AMPLIFY="{\
